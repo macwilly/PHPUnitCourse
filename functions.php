d7f6d338685afefe504121be61d1d182428e9b0b
Mackenzie Willard <mtwillard29@gmail.com>
2023-01-02 17:52:10 -0500

    New function that will add two numbers and return the sum

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Return the sum of two numbers
+ *
+ * @param int $a the first number
+ * @param int $b the second number
+ *
+ * @return int The sum of the two numbers
+ */
+function add(int $a, int $b):int
+{
+    return $a + $b;
+}
