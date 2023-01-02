9128369afe4d0db9d7935b8cc19183471b0612df
Mackenzie Willard <mtwillard29@gmail.com>
2023-01-02 17:57:43 -0500

    User require to add the function to the test case

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -2,5 +2,11 @@
 use PHPUnit\Framework\TestCase;
 class FunctionTest extends TestCase
 {
+    public function testAddReturnsTheCorrectSum()
+    {
+        //bringing the function into the test
+        require_once 'functions.php';
 
+
+    }
 }
