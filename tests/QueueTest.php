46e4408d26bae1799a853c24701cc87d62a9b279
Mackenzie Willard <mtwillard29@gmail.com>
2023-10-01 17:42:02 -0400

    Updating test methods to depend on other test methods. Also adding on PHPDoc Blocks and return information.

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,24 +4,36 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    public function testNewQueueIsEmpty()
+    /**
+     * @return Queue
+     */
+    public function testNewQueueIsEmpty():Queue
     {
         $queue = new Queue;
         $this->assertEquals(0, $queue->getCount());
+
+        return $queue;
     }
 
-    public function testAnItemIsAddedToTheQueue()
+    /**
+     * @depends testNewQueueIsEmpty
+     * @param Queue $queue
+     * @return Queue
+     */
+    public function testAnItemIsAddedToTheQueue(Queue $queue):Queue
     {
-        $queue = new Queue;
         $queue->push('green');
         $this->assertEquals(1 , $queue->getCount());
+        return $queue;
     }
 
-    public function testAnItemIsRemovedFromTheQueue()
+    /**
+     * @depends testAnItemIsAddedToTheQueue
+     * @param Queue $queue
+     * @return void
+     */
+    public function testAnItemIsRemovedFromTheQueue(Queue $queue): void
     {
-        $queue = new Queue;
-
-        $queue->push('green');
         $item = $queue->pop();
 
         $this->assertEquals(0, $queue->getCount());
