5a70ad229ec346649719788833a9c67832c5843e
Mackenzie Willard <mtwillard29@gmail.com>
2023-10-09 17:39:23 -0400

    An initial example of how mailer would be used.

--- tests/MockTest.php
+++ tests/MockTest.php
@@ -1,6 +1,14 @@
 <?php
+use PHPUnit\Framework\TestCase;
 
-class MockTest
+class MockTest extends TestCase
 {
+    public function testMock()
+    {
+        $mailer = new Mailer;
 
+        $result = $mailer->sendMessage('$david@example.com', 'Hello');
+
+        var_dump($result);
+    }
 }
