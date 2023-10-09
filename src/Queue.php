c04e169bf94ce14b1dede61cf40078ae03d4a9c3
Mackenzie Willard <mtwillard29@gmail.com>
2023-10-09 16:41:29 -0400

    Updating to the newer version on the Queue class. Will be used to test Exceptions

--- src/Queue.php
+++ src/Queue.php
@@ -7,7 +7,13 @@
  */
 class Queue
 {
-
+    
+    /**
+     * Maximum number of items in the queue
+     * @var integer
+     */
+    public const MAX_ITEMS = 5;
+    
     /**
      * Queue items
      * @var array
@@ -18,9 +24,18 @@ class Queue
      * Add an item to the end of the queue
      *
      * @param mixed $item The item
+     *
+     * @throws QueueException if the number of items on the queue exceeds
+     *                        the MAX_ITEMS value
      */
     public function push($item)
     {
+        if ($this->getCount() == static::MAX_ITEMS) {
+
+            throw new QueueException("Queue is full");
+            
+        }
+                
         $this->items[] = $item;
     }
 
@@ -42,10 +57,5 @@ class Queue
     public function getCount()
     {
         return count($this->items);
-    }
-
-    public function clear(): void
-    {
-        $this->items = [];
     }
 }
