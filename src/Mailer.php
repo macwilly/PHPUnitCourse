014d144ddb890fe9f1f40ce1e88bf8057389b2d8
Mackenzie Willard <mtwillard29@gmail.com>
2023-10-09 17:30:56 -0400

    Added method to the mailer class.

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,4 +3,17 @@
 class Mailer
 {
 
+    /**
+     * Send a message
+     *
+     * @param string $email The email address
+     * @param string $message The message
+     * @return boolean True id sent, false otherwise
+     */
+    public function sendMessage(string $email, string $message): bool
+    {
+        sleep(3);
+        echo "send '$message' to '$email'";
+        return true;
+    }
 }
